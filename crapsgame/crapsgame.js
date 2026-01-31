ab8bf445ff0838d799e471c86ab6e60ecedf77cc
quinnaza <ehinmosanayomide@gmail.com>
2026-01-31 18:40:21 +0100

    Ìmplemented dice roll animation

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -18,6 +18,8 @@ const crapsStatsUsername = "craps-stats-username"
 const crapsStatsMoney = "craps-stats-money"
 const crapsStatsRounds = "craps-stats-rounds"
 const crapsUserBetAmount = "craps-user-bet-amount"
+const crapsRollDiceButton = "craps-roll-dice-button"
+const crapsRollDiceAnimationContainer = "craps-roll-dice-animation-container"
 
 // In-game variable
 let currentMoney = startingMoney
@@ -96,4 +98,12 @@ function registerCrapsPlayer () {
   currentBetAmount = betAmount
   document.getElementById(crapsUserBetAmount).innerHTML = "$" + betAmount
   
+ }
+ function rollDice () {
+  document.getElementById(crapsRollDiceButton).style.display = "none"
+  const diceRollElement = document.getElementById(crapsRollDiceAnimationContainer)
+  rollADie({ element: diceRollElement, numberOfDice: 2, callback: processDiceResult, delay: 1000000 });
+ }
+ function processDiceResult (diceResult) {
+    console.log(diceResult)
  }
